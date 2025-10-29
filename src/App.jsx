81dfe5a2117cf1087679d1e49556e7d2ba99cd96
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2025-10-29 11:08:26 +0100

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Characters from "./components/Characters"
+
+function App() {
+
+  return (
+    <>
+
+      <Characters />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Characters.jsx
@@ -0,0 +1,34 @@
+function Characters() {
+  return (
+    <div className="characters-container">
+      
+      <div id="random">
+        <h2>Harry Potter Characters</h2>
+        <button>Find Random Character</button>
+
+        <div className="card random-card">
+          <h4>Random Character</h4>
+          <h5>Name: _______ </h5>
+          <p>Description: ________</p>
+
+          
+        </div>
+
+      </div>
+
+      <div id="fav">
+
+        <h2>Favourite Characters</h2>
+        <div className="card fav-card">
+          <h5>Name: _______ </h5>
+          <p>Description: ________</p>
+
+          <button>Remove from Fav</button>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+export default Characters
